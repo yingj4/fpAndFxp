161d06bf4e6840c59272c63f9a492d0ecd3f9345
yingj4 <yingj4@illinois.edu>
2022-07-19 16:31:02 -0500

    Comment out the error percentage checking and uncomment the FP->Int computation

--- fpToInt.py
+++ fpToInt.py
@@ -233,58 +233,61 @@ def errPercentCheck():
     print("max error_out: " + str(max(error_out)))
 
 if __name__ == '__main__':
-    # conf_8_int = fpToInt(conf_8_0)
-    # conf_9_int = fpToInt(conf_9_0)
-    # conf_10_int = fpToInt(conf_10_0)
-    # conf_11_int = fpToInt(conf_11_0)
-    # conf_12_int = fpToInt(conf_12_0)
-    # conf_13_int = fpToInt(conf_13_0)
-    # conf_14_int = fpToInt(conf_14_0)
-    # conf_15_int = fpToInt(conf_15_0)
-    # conf_16_int = fpToInt(conf_16_0)
-    # conf_17_int = fpToInt(conf_17_0)
-    # conf_18_int = fpToInt(conf_18_0)
-    # conf_19_int = fpToInt(conf_19_0)
-    # conf_20_int = fpToInt(conf_20_0)
-    # conf_21_int = fpToInt(conf_21_0)
-    # conf_22_int = fpToInt(conf_22_0)
-    # conf_23_int = fpToInt(conf_23_0)
-    # conf_24_int = fpToInt(conf_24_0)
-    # conf_25_int = fpToInt(conf_25_0)
-    #
-    # print("const int32_t conf_8 = " + str(conf_8_int) + ";\t// 08: cfg_cos_alpha;")
-    # print("const int32_t conf_9 = " + str(conf_9_int) + ";\t// 09: cfg_sin_alpha;")
-    # print("const int32_t conf_10 = " + str(conf_10_int) + ";\t// 10: cfg_cos_beta;")
-    # print("const int32_t conf_11 = " + str(conf_11_int) + ";\t// 11: cfg_sin_beta;")
-    # print("const int32_t conf_12 = " + str(conf_12_int) + ";\t// 12: cfg_cos_gamma;")
-    # print("const int32_t conf_13 = " + str(conf_13_int) + ";\t// 13: cfg_sin_gamma;")
-    # print("const int32_t conf_14 = " + str(conf_14_int) + ";\t// 14: cfg_cos_2_alpha;")
-    # print("const int32_t conf_15 = " + str(conf_15_int) + ";\t// 15: cfg_sin_2_alpha;")
-    # print("const int32_t conf_16 = " + str(conf_16_int) + ";\t// 16: cfg_cos_2_beta;")
-    # print("const int32_t conf_17 = " + str(conf_17_int) + ";\t// 17: cfg_sin_2_beta;")
-    # print("const int32_t conf_18 = " + str(conf_18_int) + ";\t// 18: cfg_cos_2_gamma;")
-    # print("const int32_t conf_19 = " + str(conf_19_int) + ";\t// 19: cfg_sin_2_gamma;")
-    # print("const int32_t conf_20 = " + str(conf_20_int) + ";\t// 20: cfg_cos_3_alpha;")
-    # print("const int32_t conf_21 = " + str(conf_21_int) + ";\t// 21: cfg_sin_3_alpha;")
-    # print("const int32_t conf_22 = " + str(conf_22_int) + ";\t// 22: cfg_cos_3_beta;")
-    # print("const int32_t conf_23 = " + str(conf_23_int) + ";\t// 23: cfg_sin_3_beta;")
-    # print("const int32_t conf_24 = " + str(conf_24_int) + ";\t// 24: cfg_cos_3_gamma;")
-    # print("const int32_t conf_25 = " + str(conf_25_int) + ";\t// 25: cfg_sin_3_gamma;")
-    #
-    # print("int32_t audio_in[128] = {")
-    # for i in range(0, 128, 16):
-    #     print(str(fpToInt(originalChannel_in_0[i])) + ', ' + str(fpToInt(originalChannel_in_0[i + 1])) + ', ' + str(fpToInt(originalChannel_in_0[i + 2])) + ', ' + str(fpToInt(originalChannel_in_0[i + 3])) + ', ' + \
-    #           str(fpToInt(originalChannel_in_0[i + 4])) + ', ' + str(fpToInt(originalChannel_in_0[i + 5])) + ', ' + str(fpToInt(originalChannel_in_0[i + 6])) + ', ' + str(fpToInt(originalChannel_in_0[i + 7])) + ', ' + \
-    #           str(fpToInt(originalChannel_in_0[i + 8])) + ', ' + str(fpToInt(originalChannel_in_0[i + 9])) + ', ' + str(fpToInt(originalChannel_in_0[i + 10])) + ', ' + str(fpToInt(originalChannel_in_0[i + 11])) + ', ' + \
-    #           str(fpToInt(originalChannel_in_0[i + 12])) + ', ' + str(fpToInt(originalChannel_in_0[i + 13])) + ', ' + str(fpToInt(originalChannel_in_0[i + 14])) + ', ' + str(fpToInt(originalChannel_in_0[i + 15])) + ', ')
-    # print("};")
-    #
-    # print("int32_t audio_out[128] = {")
-    # for i in range(0, 128, 16):
-    #     print(str(fpToInt(originalChannel_out_0[i])) + ', ' + str(fpToInt(originalChannel_out_0[i + 1])) + ', ' + str(fpToInt(originalChannel_out_0[i + 2])) + ', ' + str(fpToInt(originalChannel_out_0[i + 3])) + ', ' + \
-    #           str(fpToInt(originalChannel_out_0[i + 4])) + ', ' + str(fpToInt(originalChannel_out_0[i + 5])) + ', ' + str(fpToInt(originalChannel_out_0[i + 6])) + ', ' + str(fpToInt(originalChannel_out_0[i + 7])) + ', ' + \
-    #           str(fpToInt(originalChannel_out_0[i + 8])) + ', ' + str(fpToInt(originalChannel_out_0[i + 9])) + ', ' + str(fpToInt(originalChannel_out_0[i + 10])) + ', ' + str(fpToInt(originalChannel_out_0[i + 11])) + ', ' + \
-    #           str(fpToInt(originalChannel_out_0[i + 12])) + ', ' + str(fpToInt(originalChannel_out_0[i + 13])) + ', ' + str(fpToInt(originalChannel_out_0[i + 14])) + ', ' + str(fpToInt(originalChannel_out_0[i + 15])) + ', ')
-    # print("};")
+    # Error percentage checking
+    # errPercentCheck()
 
-    errPercentCheck()
+    # FP to Int generation
+    conf_8_int = fpToInt(conf_8_0)
+    conf_9_int = fpToInt(conf_9_0)
+    conf_10_int = fpToInt(conf_10_0)
+    conf_11_int = fpToInt(conf_11_0)
+    conf_12_int = fpToInt(conf_12_0)
+    conf_13_int = fpToInt(conf_13_0)
+    conf_14_int = fpToInt(conf_14_0)
+    conf_15_int = fpToInt(conf_15_0)
+    conf_16_int = fpToInt(conf_16_0)
+    conf_17_int = fpToInt(conf_17_0)
+    conf_18_int = fpToInt(conf_18_0)
+    conf_19_int = fpToInt(conf_19_0)
+    conf_20_int = fpToInt(conf_20_0)
+    conf_21_int = fpToInt(conf_21_0)
+    conf_22_int = fpToInt(conf_22_0)
+    conf_23_int = fpToInt(conf_23_0)
+    conf_24_int = fpToInt(conf_24_0)
+    conf_25_int = fpToInt(conf_25_0)
+
+    print("const int32_t conf_8 = " + str(conf_8_int) + ";\t// 08: cfg_cos_alpha;")
+    print("const int32_t conf_9 = " + str(conf_9_int) + ";\t// 09: cfg_sin_alpha;")
+    print("const int32_t conf_10 = " + str(conf_10_int) + ";\t// 10: cfg_cos_beta;")
+    print("const int32_t conf_11 = " + str(conf_11_int) + ";\t// 11: cfg_sin_beta;")
+    print("const int32_t conf_12 = " + str(conf_12_int) + ";\t// 12: cfg_cos_gamma;")
+    print("const int32_t conf_13 = " + str(conf_13_int) + ";\t// 13: cfg_sin_gamma;")
+    print("const int32_t conf_14 = " + str(conf_14_int) + ";\t// 14: cfg_cos_2_alpha;")
+    print("const int32_t conf_15 = " + str(conf_15_int) + ";\t// 15: cfg_sin_2_alpha;")
+    print("const int32_t conf_16 = " + str(conf_16_int) + ";\t// 16: cfg_cos_2_beta;")
+    print("const int32_t conf_17 = " + str(conf_17_int) + ";\t// 17: cfg_sin_2_beta;")
+    print("const int32_t conf_18 = " + str(conf_18_int) + ";\t// 18: cfg_cos_2_gamma;")
+    print("const int32_t conf_19 = " + str(conf_19_int) + ";\t// 19: cfg_sin_2_gamma;")
+    print("const int32_t conf_20 = " + str(conf_20_int) + ";\t// 20: cfg_cos_3_alpha;")
+    print("const int32_t conf_21 = " + str(conf_21_int) + ";\t// 21: cfg_sin_3_alpha;")
+    print("const int32_t conf_22 = " + str(conf_22_int) + ";\t// 22: cfg_cos_3_beta;")
+    print("const int32_t conf_23 = " + str(conf_23_int) + ";\t// 23: cfg_sin_3_beta;")
+    print("const int32_t conf_24 = " + str(conf_24_int) + ";\t// 24: cfg_cos_3_gamma;")
+    print("const int32_t conf_25 = " + str(conf_25_int) + ";\t// 25: cfg_sin_3_gamma;")
+
+    print("int32_t audio_in[128] = {")
+    for i in range(0, 128, 16):
+        print(str(fpToInt(originalChannel_in_0[i])) + ', ' + str(fpToInt(originalChannel_in_0[i + 1])) + ', ' + str(fpToInt(originalChannel_in_0[i + 2])) + ', ' + str(fpToInt(originalChannel_in_0[i + 3])) + ', ' + \
+              str(fpToInt(originalChannel_in_0[i + 4])) + ', ' + str(fpToInt(originalChannel_in_0[i + 5])) + ', ' + str(fpToInt(originalChannel_in_0[i + 6])) + ', ' + str(fpToInt(originalChannel_in_0[i + 7])) + ', ' + \
+              str(fpToInt(originalChannel_in_0[i + 8])) + ', ' + str(fpToInt(originalChannel_in_0[i + 9])) + ', ' + str(fpToInt(originalChannel_in_0[i + 10])) + ', ' + str(fpToInt(originalChannel_in_0[i + 11])) + ', ' + \
+              str(fpToInt(originalChannel_in_0[i + 12])) + ', ' + str(fpToInt(originalChannel_in_0[i + 13])) + ', ' + str(fpToInt(originalChannel_in_0[i + 14])) + ', ' + str(fpToInt(originalChannel_in_0[i + 15])) + ', ')
+    print("};")
+
+    print("int32_t audio_out[128] = {")
+    for i in range(0, 128, 16):
+        print(str(fpToInt(originalChannel_out_0[i])) + ', ' + str(fpToInt(originalChannel_out_0[i + 1])) + ', ' + str(fpToInt(originalChannel_out_0[i + 2])) + ', ' + str(fpToInt(originalChannel_out_0[i + 3])) + ', ' + \
+              str(fpToInt(originalChannel_out_0[i + 4])) + ', ' + str(fpToInt(originalChannel_out_0[i + 5])) + ', ' + str(fpToInt(originalChannel_out_0[i + 6])) + ', ' + str(fpToInt(originalChannel_out_0[i + 7])) + ', ' + \
+              str(fpToInt(originalChannel_out_0[i + 8])) + ', ' + str(fpToInt(originalChannel_out_0[i + 9])) + ', ' + str(fpToInt(originalChannel_out_0[i + 10])) + ', ' + str(fpToInt(originalChannel_out_0[i + 11])) + ', ' + \
+              str(fpToInt(originalChannel_out_0[i + 12])) + ', ' + str(fpToInt(originalChannel_out_0[i + 13])) + ', ' + str(fpToInt(originalChannel_out_0[i + 14])) + ', ' + str(fpToInt(originalChannel_out_0[i + 15])) + ', ')
+    print("};")
+
